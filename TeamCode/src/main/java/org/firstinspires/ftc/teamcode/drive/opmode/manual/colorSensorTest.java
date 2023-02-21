843744ee5479c3bb7b43d27f6bf8dbd116121e16
traczpasruchu <tracz.pasruchu@gmail.com>
2023-02-21 18:49:38 -0500

    Create colorSensorTest.java
    This opmode will allow us to observe the behavior of the color sensor in various situations

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/manual/colorSensorTest.java
@@ -0,0 +1,24 @@
+package org.firstinspires.ftc.teamcode.drive.opmode.manual;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+
+@TeleOp
+public class colorSensorTest extends LinearOpMode {
+    ColorSensor color;
+
+    @Override
+    public void runOpMode() {
+        color = hardwareMap.get(ColorSensor.class, "colorSensor");
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+            telemetry.addData("Red", color.red());
+            telemetry.addData("Green", color.green());
+            telemetry.addData("Blue", color.blue());
+            telemetry.update();
+        }
+    }
+}
