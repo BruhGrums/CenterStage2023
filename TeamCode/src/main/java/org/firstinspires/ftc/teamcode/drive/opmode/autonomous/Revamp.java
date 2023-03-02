bdaffd0be1949f187b77dfcc3c117f77147e1c02
SNelson2432 <nelson.craftacademy@gmail.com>
2023-03-02 16:45:07 -0500

    Update Revamp.java
    shawn low junction change

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/autonomous/Revamp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/autonomous/Revamp.java
@@ -21,7 +21,7 @@ import org.openftc.easyopencv.OpenCvWebcam;
 public class Revamp extends LinearOpMode {
     private final Pose2d startPose = new Pose2d(35, -64.25, Math.toRadians(90)); // our Starting pose allows us to know our postions of the robot and know what way it os looking at'/
     private final Pose2d stackPose = new Pose2d(49.5, -12, Math.toRadians(0));
-    private final Pose2d lowJunction = new Pose2d(31, -12, Math.toRadians(-53));
+    private final Pose2d lowJunction = new Pose2d(34, -12, Math.toRadians(-53));
     private final Pose2d medJunction = new Pose2d(35,-14, Math.toRadians(-140));
     private final Pose2d highJunction = new Pose2d(37, -12, Math.toRadians(145));
     private final double travelSpeed = 40, travelAccel = 20;
